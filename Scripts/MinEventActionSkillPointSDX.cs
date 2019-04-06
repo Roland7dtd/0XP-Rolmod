0a3bc5d267002b4768ef1cb15e5479bac26afd65
Roland7dtd <49329689+Roland7dtd@users.noreply.github.com>
2019-04-05 20:26:39 -0700

    Add files via upload

---
+++ Scripts/MinEventActionSkillPointSDX.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml;
+using UnityEngine;
+public class MinEventActionSkillPointSDX : MinEventActionRemoveBuff
+{
+    int SkillPoint = 0;
+    //  <triggered_effect trigger="onSelfBuffStart" action="SkillPointSDX, Mods" target="self" value="2" /> // two Skill points
+    public override void Execute(MinEventParams _params)
+    {
+        for (int i = 0; i < this.targets.Count; i++)
+        {
+            EntityPlayer entity = this.targets[i] as EntityPlayer;
+            if (entity != null)
+                entity.Progression.SkillPoints += this.SkillPoint;
+        }
+    }
+
+    public override bool ParseXmlAttribute(XmlAttribute _attribute)
+    {
+        bool flag = base.ParseXmlAttribute(_attribute);
+        if (!flag)
+        {
+            string name = _attribute.Name;
+            if (name != null)
+            {
+                if (name == "value" )
+                {
+                   
+                    int.TryParse(_attribute.Value, out this.SkillPoint);
+                    
+                    return true;
+                }
+            }
+        }
+        return flag;
+    }
+}
